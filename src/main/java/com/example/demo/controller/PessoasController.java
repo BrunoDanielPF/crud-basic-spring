b346063d5638cf4eb9fa4ae376a1636d77018103
Bruno Daniel <frandalozzo@gmail.com>
2022-03-04 20:27:29 -0300

    edit controller with new method http put

--- src/main/java/com/example/demo/controller/PessoasController.java
+++ src/main/java/com/example/demo/controller/PessoasController.java
@@ -2,8 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.model.PessoasModel;
 import com.example.demo.service.PessoasService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -12,23 +15,29 @@ import java.net.URISyntaxException;
 @RestController
 @RequestMapping("v1")
 public class PessoasController {
-
     @Autowired
     PessoasService pessoasService;
-    @GetMapping("/read")
-    public ResponseEntity<Iterable<PessoasModel>> retornaTodosDados(){
+    @GetMapping
+    public ResponseEntity<Iterable<PessoasModel>> returnAllPessoas(){
         return ResponseEntity.ok(pessoasService.findAll());
     }
     @GetMapping("/{id}")
     public PessoasModel returnId(@PathVariable int id){
         return pessoasService.findById(id).orElseThrow(RuntimeException::new);
     }
-    @PostMapping("/update")
-    public ResponseEntity updatePessoas(@RequestBody PessoasModel param) throws URISyntaxException {
+    @PutMapping("/{id}")
+    public ResponseEntity<PessoasModel> updatePessoa(@PathVariable int id, @Validated @RequestBody PessoasModel param) {
+        PessoasModel pessoa = pessoasService.findById(id).orElseThrow(RuntimeException::new);
+        pessoa.setNome(param.getNome());
+        final PessoasModel updatepessoa = pessoasService.save(pessoa);
+        return ResponseEntity.ok(updatepessoa);
+    }
+    @PostMapping
+    public ResponseEntity newPessoa(@RequestBody PessoasModel param) throws URISyntaxException {
        PessoasModel dadoSalvo = pessoasService.save(param);
        return ResponseEntity.created(new URI("/clients" + dadoSalvo.getId())).body(dadoSalvo);
     }
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity deletePessoa(@PathVariable int id){
         pessoasService.deleteById(id);
         return ResponseEntity.ok().build();
