4b3bcaf08a214f6c6c34b97b5b42a3546d64d6cb
Bruno Daniel <frandalozzo@gmail.com>
2022-03-21 00:17:11 -0300

    add swagger & swagger ui

--- src/main/java/com/example/demo/controller/PessoasController.java
+++ src/main/java/com/example/demo/controller/PessoasController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.dao.PessoasDao;
 import com.example.demo.model.PessoasModel;
+import com.example.demo.repository.PessoasRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,45 +12,53 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("v1")
+@Api(value = "api rest Pessoas")
 public class PessoasController {
     @Autowired
-    PessoasDao dao;
+    PessoasRepository pessoasRepository;
+
+    @ApiOperation(value = "retorna lista de pessoas")
     @GetMapping
     public ResponseEntity<Iterable<PessoasModel>> returnAllPessoas(){
         try{
-        return ResponseEntity.ok(dao.findAll());
+        return ResponseEntity.ok(pessoasRepository.findAll());
         }catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+    @ApiOperation(value = "retorna uma pessoa")
     @GetMapping("/{id}")
     public ResponseEntity returnId(@PathVariable int id){
-        return ResponseEntity.ok(dao.findById(id).orElseThrow(RuntimeException::new));
+        return ResponseEntity.ok(pessoasRepository.findById(id).orElseThrow(RuntimeException::new));
     }
+    @ApiOperation(value = "Atualiza uma pessoa")
     @PutMapping("/{id}")
     public ResponseEntity updatePessoa(@PathVariable int id, @Validated @RequestBody PessoasModel param) {
-       return dao.findById(id).map(pessoa -> {
+       return pessoasRepository.findById(id).map(pessoa -> {
             pessoa.setNome(param.getNome());
-            PessoasModel updatePessoa = dao.save(pessoa);
+            PessoasModel updatePessoa = pessoasRepository.save(pessoa);
             return ResponseEntity.ok().body(updatePessoa);
         }).orElseThrow(RuntimeException::new);
     }
+    @ApiOperation(value = "Salva uma pessoa")
     @PostMapping
     public ResponseEntity newPessoa(@RequestBody PessoasModel param) {
         try {
-            PessoasModel dadoSalvo = dao.save(param);
+            PessoasModel dadoSalvo = pessoasRepository.save(param);
             return ResponseEntity.ok().body(dadoSalvo);
         }catch (NoSuchElementException e){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
     }
+    @ApiOperation(value = "deleta uma pessoa")
     @DeleteMapping("/{id}")
     public ResponseEntity deletePessoa(@PathVariable int id){
         try{
-            dao.deleteById(id);
+            pessoasRepository.deleteById(id);
             return ResponseEntity.ok().build();
         }catch (NoSuchElementException e){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- src/main/java/com/example/demo/dao/PessoasDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.PessoasModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PessoasDao extends CrudRepository<PessoasModel, Integer> {
-}
